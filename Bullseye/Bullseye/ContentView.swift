31cf1434caceacfc6d913b206087c1bc63e4f5a8
Anthony Carrasco <carrasc132@gmail.com>
2021-10-06 17:43:22 -0700

    adding ContentView.Swift

--- Bullseye/Bullseye/ContentView.swift
+++ Bullseye/Bullseye/ContentView.swift
@@ -2,20 +2,58 @@
 //  ContentView.swift
 //  Bullseye
 //
-//  Created by Anthony Carrasco on 10/6/21.
+//  Created by Anthony Carrasco on 9/24/21.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var alertIsVisible: Bool = false
+    @State private var sliderValue: Double = 50.0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO ")
+                .bold()
+                .kerning(2.0)
+                .multilineTextAlignment(.center)
+                .lineSpacing(4.0)
+            Text("89")
+                .kerning(-1.0)
+                .font(.largeTitle)
+                .fontWeight(.black)
+            HStack {
+                Text("1")
+                    .font(.body)
+                    .bold()
+                Slider(value: self.$sliderValue, in:
+                        1.0...100.0)
+                Text("100")
+                    .font(.body)
+                    .bold()
+            }
+            Button(action: {
+                print("Hello, SwiftUI")
+                self.alertIsVisible = true
+            }) {
+                Text("Hit Me")
+            }
+            .alert(isPresented: $alertIsVisible, content: {
+                return Alert(title: Text("Hello there"), message: Text("The slide's value is \(self.sliderValue)."), dismissButton: .default(Text("Testing!")))
+            })
+        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
+            
+        Group {
+            ContentView()
+                .previewLayout(.fixed(width: 568, height: 320))
+        }
     }
 }
